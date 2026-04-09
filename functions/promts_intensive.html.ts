f157414e2264d91408ce46c622d340207f9077c7
oppfuego <zaharenkoila19@gmail.com>
2026-04-09 15:15:19 +0300

    rollback

--- functions/promts_intensive.html.ts
+++
@@ -1,4 +0,0 @@
-export const onRequest: PagesFunction = async (context) => {
-    const assetUrl = new URL("/promts_intensive", context.request.url);
-    return context.env.ASSETS.fetch(new Request(assetUrl, context.request));
-};
