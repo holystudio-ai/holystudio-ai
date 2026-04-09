b949639d9f40c64c5595e29e95c00b00b6a23cb2
oppfuego <zaharenkoila19@gmail.com>
2026-04-09 14:29:16 +0300

    fix: html page

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ import ReturnPage from "@/src/pages/ReturnPage.tsx";
 import CancelPage from "@/src/pages/CancelPage.tsx";
 import ServicePage from "@/src/pages/ServicePage.tsx";
 import EmailModal from "@/src/components/features/EmailModal.tsx";
+import PromptsIntensiveRedirect from "@/src/pages/PromptsIntensiveRedirect.tsx";
 
 const App: React.FC = () => {
     return (
@@ -30,6 +31,7 @@ const App: React.FC = () => {
                 <Route path="/intensive-smm" element={<IntensiveSmm/>}/>
                 <Route path="/intensive-photo-video" element={<IntensivePhotoVideo/>}/>
                 <Route path="/intensive" element={<Intensive/>}/>
+                <Route path="/prompts_intensive.html" element={<PromptsIntensiveRedirect/>}/>
                 <Route path="/thank-you" element={<ThankYou/>}/>
                 <Route path="/return-page" element={<ReturnPage/>}/>
                 <Route path="/cancel-page" element={<CancelPage/>}/>
