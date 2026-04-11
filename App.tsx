83928820eb8806a578fb80e4c632b622a9075149
yaroslav <yaroslav7v@gmail.com>
2026-04-11 11:40:11 +0300

    fix: restore EmailModal rendering in App.tsx

--- App.tsx
+++ App.tsx
@@ -37,6 +37,7 @@ const App: React.FC = () => {
                 <Route path="/service-page" element={<ServicePage />} />
             </Routes>
 
+            <EmailModal />
         </div>
     );
 };
